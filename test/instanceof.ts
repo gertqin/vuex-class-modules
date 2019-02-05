aa3b3550ac6633e35a15e884739ecdc62d044b6d
Bodo Graumann <mail@bodograumann.de>
2019-02-05 19:23:07 +0100

    Add test for preserving class in prototype chain

--- test/instanceof.ts
+++ test/instanceof.ts
@@ -5,17 +5,19 @@ import Vue from "vue";
 Vue.use(Vuex);
 const store = new Vuex.Store<any>({});
 
-@Module
-class MyModule extends VuexModule {
+class OriginalModule extends VuexModule {
   foo = {
     text: "some text"
   };
   bar = 1;
 }
 
+/** Manually apply decorator, to have access to initial class definition */
+const MyModule = Module(OriginalModule);
 const myModule = new MyModule({ store, name: "myModule" });
 
 test("instance of", () => {
+  expect(myModule instanceof OriginalModule).toBe(true);
   expect(myModule instanceof MyModule).toBe(true);
   expect(myModule instanceof VuexModule).toBe(true);
 });
