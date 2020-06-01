a042a6668939b8d621af909e746646389e8908bc
Liam Widdop <liamjameswiddop@gmail.com>
2020-06-02 00:59:16 +0930

    add Onload export to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { RegisterOptions, ModuleOptions } from "./module-factory";
 export { Action } from "./actions";
 export { Module } from "./module";
+export { Onload } from "./onload";
 export { Mutation } from "./mutations";
 export { VuexModule } from "./VuexModule";
