f457cf2685756632a1915655d6b3b9e480ab5dc9
Sebastian Flügge <sebastian.fluegge@faro.com>
2020-08-30 21:12:22 +0200

    Improve mutation-inheritance test
    * add second module deriving from the same base class
    * instanciate parent and both childs
    * monitor all values of all three modules in all tests

--- test/mutations-inheritance.ts
+++ test/mutations-inheritance.ts
@@ -24,47 +24,92 @@ class ParentModule extends VuexModule {
 }
 
 @Module
-class MyModule extends ParentModule {
-  baz = "init";
+class Module1 extends ParentModule {
+  private tag = "child1";
+  baz = "init" + this.tag;
 
   @Mutation
   mutation1(value: string) {
-    this.baz = value;
+    this.baz = value + this.tag;
   }
 
   @Mutation
   mutation2(value: string) {
-    this.foo = value;
+    this.foo = value + this.tag;
   }
 
   @Mutation
   mutation3(value: string) {
     super.mutation3(value);
-    this.foo = this.foo + "child";
+    this.foo = this.foo + this.tag;
+  }
+}
+
+@Module
+class Module2 extends ParentModule {
+  private tag = "child2";
+  bar = "init" + this.tag;
+  baz = "init" + this.tag;
+
+  @Mutation
+  mutation1(value: string) {
+    this.bar = value + this.tag;
+  }
+
+  @Mutation
+  mutation2(value: string) {
+    this.baz = value + this.tag;
+  }
+
+  @Mutation
+  mutation3(value: string) {
+    super.mutation3(value);
+    this.foo = this.foo + this.tag;
   }
 }
 
 describe("mutations-inheritance", () => {
   let store: Store<any>;
-  let myModule: MyModule;
+  let parent: ParentModule;
+  let child1: Module1;
+  let child2: Module2;
 
   beforeEach(() => {
     store = new Vuex.Store({});
-    myModule = new MyModule({ store, name: "myModule" });
+    parent = new ParentModule({ store, name: "parentModule" });
+    child1 = new Module1({ store, name: "myModule1" });
+    child2 = new Module2({ store, name: "myModule2" });
   });
 
   test("overriden mutation can modify state", () => {
-    myModule.mutation1("bar");
-    expect(myModule.baz).toBe("bar");
+    parent.mutation1("_");
+    child1.mutation1("bar1");
+    child2.mutation1("bar2");
+    expect(parent.foo).toBe("init");
+    expect(child1.baz).toBe("bar1child1");
+    expect(child2.bar).toBe("bar2child2");
+    expect(child2.baz).toBe("initchild2");
   });
 
   test("overriden mutation can modify parent state", () => {
-    myModule.mutation2("bar");
-    expect(myModule.foo).toBe("bar");
+    parent.mutation2("_");
+    child1.mutation2("bar");
+    child2.mutation2("baz");
+    expect(parent.foo).toBe("init");
+    expect(child1.foo).toBe("barchild1");
+    expect(child2.bar).toBe("initchild2");
+    expect(child2.baz).toBe("bazchild2");
   });
 
   test("overriden mutation has access to parent method implementation", () => {
-    myModule.mutation3("bar");
-    expect(myModule.foo).toBe("barchild");
+    parent.mutation3("foo_");
+    child1.mutation3("foo1");
+    child2.mutation3("foo2");
+    expect(parent.foo).toBe("foo_");
+    expect(child1.foo).toBe("foo1child1");
+    expect(child1.baz).toBe("initchild1");
+    expect(child2.foo).toBe("foo2child2");
+    expect(child2.bar).toBe("initchild2");
+    expect(child2.baz).toBe("initchild2");
   });
 });
